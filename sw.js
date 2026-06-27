87148a6adb83569cb1a1915362fa57cfc9da7770
diegolillton <diegolilton@gmail.com>
2026-06-26 21:48:06 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'ferramentas-v1';
+const ARQUIVOS = [
+  '/ferramentas/catalogo.html',
+  '/ferramentas/lexico.html',
+  '/ferramentas/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ARQUIVOS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).then(resp => {
+      const clone = resp.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return resp;
+    }).catch(() => cached))
+  );
+});
